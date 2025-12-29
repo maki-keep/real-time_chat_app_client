6abd5ab4f9bf64f7c049e5d2646409a2178643c7
Nishikino Maki <maki.keep@gmail.com>
2025-12-29 13:41:39 +0000

    Create homepage and Tailwind CSS
    Adds Tailwind CSS. All CSS rulesets must be in index.css.
    Updates project to Real-Time Chat App, with a homepage that displays buttons Sign In and Sign Up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="bg-app-bg flex flex-col gap-8 items-center justify-center min-h-screen text-app-text">
+      <div className="flex gap-8 items-center">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img
+            src={viteLogo}
+            className="h-24 p-6"
+            alt="Vite logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1 className="font-semibold text-4xl">Real-Time Chat App</h1>
+      <div className="bg-app-card flex flex-col gap-8 justify-center p-8 rounded-lg shadow-md">
+        <button className="bg-app-btn border border-app-btn-border focus:outline-none focus-visible:ring-4 focus-visible:ring-app-accent/30 font-medium hover:border-app-accent px-8 py-4 rounded-lg text-xl">
+          Sign In
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button className="bg-app-btn border border-app-btn-border focus:outline-none focus-visible:ring-4 focus-visible:ring-app-accent/30 font-medium hover:border-app-accent px-8 py-4 rounded-lg text-xl">
+          Sign Up
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="mt-4 text-app-muted">
+        Powered by Vite.
       </p>
-    </>
+    </div>
   )
 }
 
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  darkMode: 'media',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'app-bg': 'var(--app-bg)',
+        'app-text': 'var(--app-text)',
+        'app-muted': 'var(--app-muted)',
+        'app-card': 'var(--app-card)',
+        'app-btn': 'var(--app-btn)',
+        'app-btn-border': 'var(--app-btn-border)',
+        'app-accent': 'var(--app-accent)'
+      }
+    }
+  },
+  plugins: []
+}
